0e183d9a7f468f84f151151627d1105f6e870807
NITHEESHWAR B R <95460968+NitheeshwarBR@users.noreply.github.com>
2022-02-09 03:47:10 +0000

    p2original.c

--- p2original.c
+++ p2original.c
@@ -14,7 +14,25 @@ int check_scalene(int a,int b,int c)
   }
 
 }
-void output(int a,int b,int scalene)
+void output(int a,int b,int c,int scalene)
 {
-  
+  if(scalene==0)
+  {
+    printf("the triangle with sides %d , %d and %d is scalene\n",a,b,c);
+  }
+  else
+  {
+    printf("it is not a scalene triangle\n");
+  }
+
+}
+int main()
+{
+  int a,b,c,result;
+  a=input();
+  b=input();
+  c=input();
+  result=check_scalene(a,b,c);
+  output(a,b,c,result);
+  return 0;
 }
